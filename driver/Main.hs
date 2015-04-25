b25fa0f2ab6ab16d311912937eb01b9db4ac778b
Simon Hengel <sol@typeful.net>
2015-04-25 12:45:53 +0800

    Only write cabal file if there are changes

--- driver/Main.hs
+++ driver/Main.hs
@@ -1,6 +1,16 @@
 module Main (main) where
 
+import           Prelude ()
+import           Prelude.Compat
+import           Control.Monad
+import           Control.DeepSeq
+import           Control.Exception
+import           System.IO.Error
+
 import           Run
 
 main :: IO ()
-main = run >>= uncurry writeFile
+main = do
+  (name, new) <- run
+  old <- force . either (const Nothing) Just <$> tryJust (guard . isDoesNotExistError) (readFile name)
+  unless (old == Just new) (writeFile name new)
