a24d196263ec0434c49cd02c0ee7e06262c0e482
Simon Hengel <sol@typeful.net>
2015-06-12 12:20:30 +0800

    Depend on mockery >= 0.3

--- test/Helper.hs
+++ test/Helper.hs
@@ -2,16 +2,8 @@ module Helper (
   module Test.Hspec
 , module Test.Mockery.Directory
 , module Control.Applicative
-, touch
 ) where
 
 import           Test.Hspec
 import           Test.Mockery.Directory
 import           Control.Applicative
-import           System.Directory
-import           System.FilePath
-
-touch :: FilePath -> IO ()
-touch p = do
-  createDirectoryIfMissing True (takeDirectory p)
-  writeFile p ""
