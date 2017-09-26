a0a41e2af90a34d67d4bb83313a6622db2852ae5
Simon Hengel <sol@typeful.net>
2017-09-26 18:39:53 +0800

    Refactoring of tests for dependency parsing

--- test/Hpack/DependencySpec.hs
+++ test/Hpack/DependencySpec.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE QuasiQuotes #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE OverloadedLists #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ConstraintKinds #-}
@@ -8,7 +7,6 @@ module Hpack.DependencySpec (spec) where
 import           Helper
 import           Test.HUnit
 
-import           Data.Aeson.QQ
 import           Data.Aeson.Types
 import qualified Data.Map.Lazy as Map
 import           Data.String.Interpolate.IsString
@@ -17,77 +15,16 @@ import           Data.ByteString (ByteString)
 
 import           Hpack.Dependency
 
+parsesAs :: HasCallStack => ByteString -> Either String [(String, DependencyVersion)] -> Expectation
+parsesAs input expected = do
+  value <- either assertFailure return (decodeEither input)
+  parseEither parseJSON value `shouldBe` (Dependencies . Map.fromList <$> expected)
+
+
 spec :: Spec
 spec = do
   describe "parseJSON" $ do
-    context "when parsing a Dependency" $ do
-      context "when parsing source dependencies" $ do
-        it "accepts git dependencies" $ do
-          let value = [aesonQQ|{
-                name: "hpack",
-                git: "https://github.com/sol/hpack",
-                ref: "master"
-              }|]
-              source = GitRef "https://github.com/sol/hpack" "master" Nothing
-          parseEither parseJSON value `shouldBe` Right (Dependency "hpack" (SourceDependency source))
-
-        it "accepts github dependencies" $ do
-          let value = [aesonQQ|{
-                name: "hpack",
-                github: "sol/hpack",
-                ref: "master"
-              }|]
-              source = GitRef "https://github.com/sol/hpack" "master" Nothing
-          parseEither parseJSON value `shouldBe` Right (Dependency "hpack" (SourceDependency source))
-
-        it "accepts an optional subdirectory for git dependencies" $ do
-          let value = [aesonQQ|{
-                name: "warp",
-                github: "yesodweb/wai",
-                ref: "master",
-                subdir: "warp"
-              }|]
-              source = GitRef "https://github.com/yesodweb/wai" "master" (Just "warp")
-          parseEither parseJSON value `shouldBe` Right (Dependency "warp" (SourceDependency source))
-
-        it "accepts local dependencies" $ do
-          let value = [aesonQQ|{
-                name: "hpack",
-                path: "../hpack"
-              }|]
-              source = Local "../hpack"
-          parseEither parseJSON value `shouldBe` Right (Dependency "hpack" (SourceDependency source))
-
-        context "when parsing fails" $ do
-          it "returns an error message" $ do
-            let value = Number 23
-            parseEither parseJSON value `shouldBe` (Left "Error in $: expected String or an Object, encountered Number" :: Either String Dependency)
-
-          context "when ref is missing" $ do
-            it "produces accurate error messages" $ do
-              let value = [aesonQQ|{
-                    name: "hpack",
-                    git: "sol/hpack",
-                    ef: "master"
-                  }|]
-              parseEither parseJSON value `shouldBe` (Left "Error in $: key \"ref\" not present" :: Either String Dependency)
-
-          context "when both git and github are missing" $ do
-            it "produces accurate error messages" $ do
-              let value = [aesonQQ|{
-                    name: "hpack",
-                    gi: "sol/hpack",
-                    ref: "master"
-                  }|]
-              parseEither parseJSON value `shouldBe` (Left "Error in $: neither key \"git\" nor key \"github\" present" :: Either String Dependency)
-
     context "when parsing Dependencies" $ do
-      let
-        parsesAs :: HasCallStack => ByteString -> Either String [(String, DependencyVersion)] -> Expectation
-        parsesAs input expected = do
-          value <- either assertFailure return (decodeEither input)
-          parseEither parseJSON value `shouldBe` (Dependencies . Map.fromList <$> expected)
-
       context "with a scalar" $ do
         it "accepts dependencies without constraints" $ do
           [i|
@@ -116,43 +53,86 @@ spec = do
             - hpack >= 2 && < 3
           |] `parsesAs` Right [("hpack", VersionRange ">=2 && <3")]
 
-        it "accepts an array of hashes" $ do
+        it "accepts git dependencies" $ do
+          let source = GitRef "https://github.com/sol/hpack" "master" Nothing
+          [i|
+            - name: hpack
+              git: https://github.com/sol/hpack
+              ref: master
+          |] `parsesAs` Right [("hpack", SourceDependency source)]
+
+        it "accepts github dependencies" $ do
+          let source = GitRef "https://github.com/sol/hpack" "master" Nothing
+          [i|
+            - name: hpack
+              github: sol/hpack
+              ref: master
+          |] `parsesAs` Right [("hpack", SourceDependency source)]
+
+        it "accepts an optional subdirectory for git dependencies" $ do
+          let source = GitRef "https://github.com/yesodweb/wai" "master" (Just "warp")
+          [i|
+            - name: warp
+              github: yesodweb/wai
+              ref: master
+              subdir: warp
+          |] `parsesAs` Right [("warp", SourceDependency source)]
+
+        it "accepts local dependencies" $ do
+          let source = Local "../hpack"
           [i|
             - name: hpack
               path: ../hpack
-          |] `parsesAs` Right [("hpack", SourceDependency (Local "../hpack"))]
+          |] `parsesAs` Right [("hpack", SourceDependency source)]
 
-      context "with a hash" $ do
+        context "when ref is missing" $ do
+          it "produces accurate error messages" $ do
+            [i|
+              - name: hpack
+                git: sol/hpack
+                ef: master
+            |] `parsesAs` Left "Error in $[0]: key \"ref\" not present"
+
+        context "when both git and github are missing" $ do
+          it "produces accurate error messages" $ do
+            [i|
+              - name: hpack
+                gi: sol/hpack
+                ref: master
+            |] `parsesAs` Left "Error in $[0]: neither key \"git\" nor key \"github\" present"
+
+      context "with a mapping from dependency names to constraints" $ do
         it "accepts dependencies without constraints" $ do
           [i|
             array:
           |] `parsesAs` Right [("array", AnyVersion)]
 
-        it "accepts dependencies with constraints" $ do
+        it "rejects invalid values" $ do
           [i|
-            bytestring: 0.10.8.2
-          |] `parsesAs` Right [("bytestring", VersionRange "0.10.8.2")]
+            hpack: 23
+          |] `parsesAs` Left "Error in $.hpack: expected Null, Object, or String, encountered Number"
 
-        it "accepts a hash of hashes" $ do
-          [i|
-            Cabal:
-              github: haskell/cabal
-              ref: d53b6e0d908dfedfdf4337b2935519fb1d689e76
-              subdir: Cabal
-          |] `parsesAs` Right [("Cabal", SourceDependency (GitRef "https://github.com/haskell/cabal" "d53b6e0d908dfedfdf4337b2935519fb1d689e76" (Just "Cabal")))]
-
-        it "ignores names in nested hashes" $ do
-          [i|
-            outer-name:
-              name: inner-name
-              path: somewhere
-          |] `parsesAs` Right [("outer-name", SourceDependency (Local "somewhere"))]
-
-        context "with invalid value" $ do
-          it "returns an error message" $ do
+        context "when the constraint is a String" $ do
+          it "accepts version ranges" $ do
             [i|
-              hpack: 23
-            |] `parsesAs` Left "Error in $.hpack: expected Null, Object, or String, encountered Number"
+              hpack: '>=2'
+            |] `parsesAs` Right [("hpack", VersionRange ">=2")]
+
+        context "when the constraint is an Object" $ do
+          it "accepts github dependencies" $ do
+            [i|
+              Cabal:
+                github: haskell/cabal
+                ref: d53b6e0d908dfedfdf4337b2935519fb1d689e76
+                subdir: Cabal
+            |] `parsesAs` Right [("Cabal", SourceDependency (GitRef "https://github.com/haskell/cabal" "d53b6e0d908dfedfdf4337b2935519fb1d689e76" (Just "Cabal")))]
+
+          it "ignores names in nested hashes" $ do
+            [i|
+              outer-name:
+                name: inner-name
+                path: somewhere
+            |] `parsesAs` Right [("outer-name", SourceDependency (Local "somewhere"))]
 
   describe "parseDependency" $ do
     it "parses a dependency" $ do
