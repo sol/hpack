10b1aa26a1c4fae9cc5f1d9b50a537b1cd173d4c
Simon Hengel <sol@typeful.net>
2018-02-05 22:54:03 +0800

    Minor fixup

--- get-hpack.sh
+++ get-hpack.sh
@@ -8,11 +8,11 @@ hpack="$dst/hpack"
 mkdir -p "$dst"
 
 os="${1:-${TRAVIS_OS_NAME:-linux}}"
-url=$(curl -s https://api.github.com/repos/sol/hpack/releases/latest | jq -r ".assets[] | select(.name | test(\"$os\")) | .browser_download_url")
+url=$(curl -sSL https://api.github.com/repos/sol/hpack/releases/latest | jq -r ".assets[] | select(.name | test(\"$os\")) | .browser_download_url")
 
 echo "Downloading $url"
 
-curl -sL "$url" | gunzip > "$hpack.tmp"
+curl -sSL "$url" | gunzip > "$hpack.tmp"
 chmod +x "$hpack.tmp"
 mv "$hpack.tmp" "$hpack"
 
