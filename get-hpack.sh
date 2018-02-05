b1106ae81c1a836f147e4ecbda82e7ad229f8184
Simon Hengel <sol@typeful.net>
2018-02-05 22:33:51 +0800

    Add get-hpack.sh (see #256)

---
+++ get-hpack.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+dst="$HOME/.local/bin"
+hpack="$dst/hpack"
+
+mkdir -p "$dst"
+
+os="${1:-${TRAVIS_OS_NAME:-linux}}"
+url=$(curl -s https://api.github.com/repos/sol/hpack/releases/latest | jq -r ".assets[] | select(.name | test(\"$os\")) | .browser_download_url")
+
+echo "Downloading $url"
+
+curl -sL "$url" | gunzip > "$hpack.tmp"
+chmod +x "$hpack.tmp"
+mv "$hpack.tmp" "$hpack"
+
+echo "Installed hpack to $hpack"
