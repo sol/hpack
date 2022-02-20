39047cf6927f87081842ea35023d2f0b37473e5c
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-20 14:40:02 +0800

    Delete src directory

--- src/Data/Aeson/Config/KeyMap.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE CPP #-}
-module Data.Aeson.Config.KeyMap (module KeyMap) where
-
-#if MIN_VERSION_aeson(2,0,0)
-import           Data.Aeson.KeyMap as KeyMap
-#else
-import           Data.HashMap.Strict as KeyMap
-#endif
--- src/Data/Aeson/Config/Util.hs
+++
@@ -1,8 +0,0 @@
-module Data.Aeson.Config.Util where
-
-import           Data.Aeson.Types (camelTo2)
-
-hyphenize :: String -> String -> String
-hyphenize name = camelTo2 '-' . dropPrefix . dropWhile (== '_')
-  where
-    dropPrefix = drop (length (dropWhile (== '_') $ reverse name))
