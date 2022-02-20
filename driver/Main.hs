5ca2b92a8095fbc459161ab58deeee1edc29e25b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-20 14:39:42 +0800

    Delete driver directory

--- driver/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main (main) where
-
-import           System.Environment
-
-import qualified Hpack
-import qualified Hpack.Config as Hpack
-
-main :: IO ()
-main = getArgs >>= Hpack.getOptions Hpack.packageConfig >>= mapM_ (uncurry Hpack.hpack)
