d4635054ecde02692eff080e515cbd70968f38f2
Hécate Moonlight <hecate+github@glitchbra.in>
2023-09-18 13:58:10 +0200

    Hide Data.List.List import

--- src/Imports.hs
+++ src/Imports.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 module Imports (module Imports) where
 
 import           Control.Applicative as Imports
@@ -6,7 +7,11 @@ import           Control.Exception as Imports (Exception(..))
 import           Control.Monad as Imports
 import           Control.Monad.IO.Class as Imports
 import           Data.Bifunctor as Imports
+#if MIN_VERSION_base(4,20,0)
+import           Data.List as Imports hiding (List, sort, nub)
+#else
 import           Data.List as Imports hiding (sort, nub)
+#endif
 import           Data.Monoid as Imports (Monoid(..))
 import           Data.Semigroup as Imports (Semigroup(..))
 import           Data.String as Imports
